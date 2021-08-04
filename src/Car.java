adb7c19a053a4e595c4535b867694136b1a3ef02
ferhanali <Bronx_10460>
2021-08-03 20:45:38 -0400

    Third commit

---
+++ src/Car.java
@@ -0,0 +1,23 @@
+public class Car {
+    String make;
+    String  model;
+    int year;
+    String color;
+
+    // Custom Constructor
+    public Car(String make, String model, int year, String color){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+    public static void main(String[] args) {
+
+        Car bmw = new Car("Bmw", "X5", 2021, "Black");
+
+        System.out.println(bmw.color);
+    }
+    public void speedUp(){
+        System.out.println("speeding ...");
+    }
+}
---
+++ src/Iphone.java
@@ -0,0 +1,17 @@
+public class Iphone {
+
+    public static void main(String[] args) {
+
+
+        sendText("Hello");
+        makeCall("0123456789");
+
+    }
+    public static void sendText(String message){
+        System.out.println(message);
+    }
+    public static void makeCall(String number){
+        System.out.println(number);
+
+    }
+}
